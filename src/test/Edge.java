5e3fd546a9fadc6cd41cbc81a2ee5b40fcaaf236
Anderson de França Queiroz <contato@andersonq.eti.br>
2012-10-31 09:15:41 -0200

    A Edge

---
+++ src/test/Edge.java
@@ -0,0 +1,15 @@
+package test;
+
+import unionfind.Node;
+
+/**
+ * 
+ * @author Anderson Queiroz <contato(at)andersonq(dot)eti(dot)br
+ *
+ */
+public class Edge {
+
+	Node u;
+	Node v;
+	int cost;
+}
